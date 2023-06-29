7f7767eab8b2d73836fb618b39a2751ced5b4fea
MICcall2004 <sseony88@gmail.com>
2023-06-29 15:49:07 +0900

    062902

--- book/Car.java
+++ book/Car.java
@@ -1,35 +1,25 @@
+// gas 값이 0보다 클 경우 계속해서 while 문을 실행하고, 0일 경우 return 문을 실행해서 run() 메소드를 즉시 종료한다. while 문이 한 번 루핑될 때마다 gas 를 1씩 감소하기 때문에 언젠가는 0이 되어 rn() 메소드를 종료한다. return 문 대신 break 문을 사용할 수도 있다. 
+
 package book;
 
 public class Car {
     // 필드
-    int gas;
+    String model;
+    int speed;
 
     // 생성자
-
-    // 메소드 
-    void setGas(int gas) {
-        this.gas = gas;
+    Car(String model) {
+        this.model = model;
     }
 
-    boolean isLeftGas() {
-        if (gas == 0) {
-            System.out.println("gas가 없습니다. ");
-            return false;
-        }
-
-        System.out.println("gas가 있습니다. ");
-        return true;
+    void setSpeed(int speed) {
+        this.speed = speed;
     }
 
     void run() {
-        while (true) {
-            if (gas > 0) {
-                System.out.println("달립니다. (gas잔량:" + gas + ")");
-                gas -= 1;
-            } else {
-                System.out.println("멈춥니다. (gas잔량: " + gas + ")");
-                return;
-            }
+        for (int i = 10; i <= 50; i += 10) {
+            setSpeed(i);
+            System.out.println(this.model + "가 달립니다. (시속:" + this.speed + "km/h)");
         }
     }
 }
--- book/CarExample.java
+++ book/CarExample.java
@@ -2,20 +2,10 @@ package book;
 
 public class CarExample {
     public static void main(String[] args) {
-        Car myCar = new Car();
+        Car myCar = new Car("포르쉐");
+        Car youCar = new Car("벤츠");
 
-        myCar.setGas(5); // Car의 SetGas() 메소드 호출
-
-        boolean gasState = myCar.isLeftGas(); // Car의 isLeftGas() 메소드 호출
-        if (gasState) {
-            System.out.println("출발합니다. ");
-            myCar.run();
-        }
-
-        if (myCar.isLeftGas()) {
-            System.out.println("gas를 주입할 필요가 없습니다. ");
-        } else {
-            System.out.println("gas를 주입하세요.");
-        }
+        myCar.run();
+        youCar.run();
     }
 }
